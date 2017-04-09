7d91b06e936315fc353c4cf1850d1fe42f73b4ad
CollinHemeltjen <collinhemeltjen@gmail.com>
2017-04-09 11:27:38 +0200

    Huidige website
    Huidige website toegevoegd

---
+++ scripts.js
@@ -0,0 +1,21 @@
+
+
+function showToelichting(x) {
+    toelichting = document.getElementById(x);
+    var status = toelichting.style.display;
+    if(status==="block"){
+        toelichting.style.display = "none";
+        isAan = true;
+    }else{
+        toelichting.style.display = "block";
+        isAan = false;
+    }
+}
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-96566267-1', 'auto');
+  ga('send', 'pageview');
+
